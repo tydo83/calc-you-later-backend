99de6cd2691c0387cf09246811a410727b45ef48
Timothy Do <tydo83@gmail.com>
2020-10-21 15:29:33 -0400

    first version

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,26 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
-
- 
+const calculate = function(num1, num2, operator) {
+  if(operator === '+' || operator === 'plus' || operator === 'added to') {
+    return Number(num1) + Number(num2);
+  } 
+  else if(operator === '-' || operator === 'minus' || operator === 'subtracted from') {
+    return Number(num1) - Number(num2);
+  }
+  else if(operator.toLowerCase() === 'x' || operator === 'times' || operator === 'multiplied by') {
+    return Number(num1) * Number(num2);
+  }
+  else if(operator === '/' || operator === 'divided by') {
+    return Number(num1) / Number(num2);
+  }
+  else if(operator === '%' || operator === 'modulus' || operator === 'mod') {
+    return Number(num1) % Number(num2);
+  }
+  else {
+    return `Sorry, that's not a mathematical operation!`;
+  }
+}
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
